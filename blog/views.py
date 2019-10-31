0ac7eb44bc2a16156a7f5c27683d64b2405c8745
YUTING LI <524290558@qq.com>
2019-10-31 14:35:32 +0800

    update

--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,4 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def post_list(request):
-    # return HttpResponse('一个二恶')
     return render(request, 'blog/post_list.html', {})
